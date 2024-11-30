faeeba3010b9751ee3fa835b4308722f926af808
rayark2016 <47951425+rayark2016@users.noreply.github.com>
2024-12-01 02:16:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from flask import Flask, request, abort
-
-from linebot.v3 import (
-    WebhookHandler
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
+from flask import Flask, request, abort, url_for
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
     Configuration,
     ApiClient,
     MessagingApi,
-    ReplyMessageRequest,
-    PushMessageRequest,
-    BroadcastRequest,
-    MulticastRequest,
-    TextMessage,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TemplateMessage,
-    ButtonsTemplate,
-    PostbackAction,
     ReplyMessageRequest,
     TextMessage,
     Emoji,
@@ -32,35 +16,27 @@ from linebot.v3.messaging import (
 )
 from linebot.v3.webhooks import (
     MessageEvent,
-    TextMessageContent,
-    PostbackEvent,
     TextMessageContent
-
 )
 import os
 
 app = Flask(__name__)
 
-from linebot import WebhookHandler
-import os
+# 環境變數設置
+configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN', ''))
+if not configuration.access_token:
+    raise ValueError("Missing environment variable: CHANNEL_ACCESS_TOKEN")
 
-
-
-
-configuration = Configuration(access_token = os.getenv('CHANNEL_ACCESS_TOKEN'))
-line_handler = WebhookHandler (os.getenv('CHANNEL_SECRET'))
-
+line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET', ''))
+if not line_handler.channel_secret:
+    raise ValueError("Missing environment variable: CHANNEL_SECRET")
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:
@@ -69,6 +45,9 @@ def callback():
 
     return 'OK'
 
+def get_static_url(filename):
+    url = url_for('static', filename=filename, _external=True)
+    return url.replace("http", "https")
 
 @line_handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
@@ -80,78 +59,22 @@ def handle_message(event):
             line_bot_api.reply_message(
                 ReplyMessageRequest(
                     reply_token=event.reply_token,
-                    messages=[TextMessage(text="這是文字訊息")]
-                )
-            )
-        elif text == '表情符號':
-            emojis = [
-                Emoji(index=0, product_id="5ac1bfd5040ab15980c9b435", emoji_id="001"),
-                Emoji(index=12, product_id="5ac1bfd5040ab15980c9b435", emoji_id="002")
-            ]
-            line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text='$ LINE 表情符號 $', emojis=emojis)]
-                )
-            )
-        elif text == '貼圖':
-            line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[StickerMessage(package_id="446", sticker_id="1988")]
+                    messages=[TextMessage(text="這是文字訊息喔")]
                 )
             )
         elif text == '圖片':
-            url = request.url_root + 'static/Logo.jpg'
-            url = url.replace("http", "https")
-            app.logger.info("url=" + url)
+            url = get_static_url('Logo.jpg')
             line_bot_api.reply_message(
                 ReplyMessageRequest(
                     reply_token=event.reply_token,
-                    messages=[
-                        ImageMessage(original_content_url=url, preview_image_url=url)
-                    ]
-                )
-            )
-        elif text == '影片':
-            url = request.url_root + 'static/video.mp4'
-            url = url.replace("http", "https")
-            app.logger.info("url=" + url)
-            line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[
-                        VideoMessage(original_content_url=url, preview_image_url=url)
-                    ]
-                )
-            )
-        elif text == '音訊':
-            url = request.url_root + 'static/music.mp3'
-            url = url.replace("http", "https")
-            app.logger.info("url=" + url)
-            duration = 60000  # in milliseconds
-            line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[
-                        AudioMessage(original_content_url=url, duration=duration)
-                    ]
-                )
-            )
-        elif text == '位置':
-            line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[
-                        LocationMessage(title='Location', address="Taipei", latitude=25.0475, longitude=121.5173)
-                    ]
+                    messages=[ImageMessage(original_content_url=url, preview_image_url=url)]
                 )
             )
         else:
             line_bot_api.reply_message(
                 ReplyMessageRequest(
                     reply_token=event.reply_token,
-                    messages=[TextMessage(text=event.message.text)]
+                    messages=[TextMessage(text="我不太明白你的意思，可以試試其他指令，例如：文字、圖片")]
                 )
             )
 
